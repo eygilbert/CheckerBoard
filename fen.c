b8c71541b3a44434f912c877d31862167a0e0ee8
eygilbert <eygilbert@gmail.com>
2019-11-06 07:24:05 -0500

    Fixed problem with fen parser not accepting a position with no pieces of one color.

--- fen.c
+++ fen.c
@@ -9,7 +9,7 @@
 /*
  * Return true if the string looks like a fen position.
  */
-int is_fen(char *buf)
+int is_fen(const char *buf)
 {
 	while (*buf) {
 		if (isspace(*buf))
@@ -33,12 +33,12 @@ int is_fen(char *buf)
  *		- Don't expect a "." at the end of the FEN string.
  *		- Accept square number ranges. ex: B:B1-12:W21-32
  */
-int FENtoboard8(Board8x8 board, char *buf, int *poscolor, int gametype)
+int FENtoboard8(Board8x8 board, const char *buf, int *poscolor, int gametype)
 {
 	int square, square2, s;
-	int color, piece;
+	int color, piece_type;
 	int i, j;
-	char *lastp;
+	const char *lastp;
 
 	/* Allow possible extraneous stuff at the beginning, since it is used by the clipboard paste handler. */
 	lastp = strchr(buf, ':');
@@ -61,40 +61,51 @@ int FENtoboard8(Board8x8 board, char *buf, int *poscolor, int gametype)
 	}
 
 	++buf;
+	if (*buf != ':')
+		return(0);
+
+	++buf;
 	lastp = buf;
 	while (*buf) {
-		if (*buf == ':') {
+		piece_type = CB_MAN;
+		if (*buf == '"') {
 			++buf;
-			if (toupper(*buf) == 'W')
-				color = CB_WHITE;
-			else if (toupper(*buf) == 'B')
-				color = CB_BLACK;
-			else
-				return(0);
+			continue;
+		}
+		if (std::toupper(*buf) == 'W') {
+			color = CB_WHITE;
+			++buf;
+			continue;
+		}
+		if (std::toupper(*buf) == 'B') {
+			color = CB_BLACK;
+			++buf;
+			continue;
+		}
+		if (std::toupper(*buf) == 'K') {
+			piece_type = CB_KING;
 			++buf;
 		}
-
-		piece = CB_MAN;
-		if (toupper(*buf) == 'K') {
-			piece = CB_KING;
-			++buf;
-		}
-
-		for (square = 0; isdigit(*buf); ++buf)
+		for (square = 0; std::isdigit(*buf); ++buf)
 			square = 10 * square + (*buf - '0');
 
-		/* Check for range of square numbers. */
 		square2 = square;
-		if (*buf == '-' && isdigit(buf[1])) {
+		if (*buf == ',' || *buf == ':')
 			++buf;
-			for (square2 = 0; isdigit(*buf); ++buf)
+
+		else if (*buf == '-' && std::isdigit(buf[1])) {
+			++buf;
+			for (square2 = 0; std::isdigit(*buf); ++buf)
 				square2 = 10 * square2 + (*buf - '0');
+			if (*buf == ',' || *buf == ':')
+				++buf;
 		}
+
 
 		if (square && square <= square2) {
 			for (s = square; s <= square2; ++s) {
 				numbertocoors(s, &i, &j, gametype);
-				board[i][j] = piece | color;
+				board[i][j] = piece_type | color;
 			}
 		}
 
@@ -110,48 +121,49 @@ int FENtoboard8(Board8x8 board, char *buf, int *poscolor, int gametype)
 	return(1);
 }
 
-void board8toFEN(Board8x8 board, char *p, int color, int gametype)
+void board8toFEN(const Board8x8 board, std::string &fenstr, int color, int gametype)
 {
-	int i, j, number;
-	char s[256];
+	int i, j, square;
 
-	/* prints a FEN string into p derived from board */
-	/* sample FEN string: "W:W18,20,23,K25:B2,6,9" */
-	sprintf(p, "");
+	fenstr = color == CB_BLACK ? "B" : "W";
 
-	if (color == CB_BLACK)
-		sprintf(s, "B:W");
-	else
-		sprintf(s, "W:W");
-	strcat(p, s);
+	/* Add the white pieces. */
+	fenstr += ":W";
 	for (j = 0; j <= 7; j++) {
 		for (i = 7; i >= 0; i--) {
-			sprintf(s, "");
-			number = coorstonumber(i, j, gametype);
+			square = coorstonumber(i, j, gametype);
 			if (board[i][j] == (CB_WHITE | CB_MAN))
-				sprintf(s, "%i,", number);
+				fenstr += std::to_string(square) + ",";
 			if (board[i][j] == (CB_WHITE | CB_KING))
-				sprintf(s, "K%i,", number);
-			strcat(p, s);
+				fenstr += "K" + std::to_string(square) + ",";
 		}
 	}
 
 	/* remove last comma */
-	p[strlen(p) - 1] = 0;
-	sprintf(s, ":B");
-	strcat(p, s);
+	if (fenstr[fenstr.size() - 1] == ',')
+		fenstr.pop_back();
+
+	/* Add the black pieces. */
+	fenstr += ":B";
 	for (j = 0; j <= 7; j++) {
 		for (i = 7; i >= 0; i--) {
-			sprintf(s, "");
-			number = coorstonumber(i, j, gametype);
+			square = coorstonumber(i, j, gametype);
 			if (board[i][j] == (CB_BLACK | CB_MAN))
-				sprintf(s, "%i,", number);
+				fenstr += std::to_string(square) + ",";
 			if (board[i][j] == (CB_BLACK | CB_KING))
-				sprintf(s, "K%i,", number);
-			strcat(p, s);
+				fenstr += "K" + std::to_string(square) + ",";
 		}
 	}
 
 	/* remove last comma */
-	p[strlen(p) - 1] = 0;
+	if (fenstr[fenstr.size() - 1] == ',')
+		fenstr.pop_back();
 }
+
+void board8toFEN(const Board8x8 board, char *fenstr, int color, int gametype)
+{
+	std::string fen;
+	board8toFEN(board, fen, color, gametype);
+	strcpy(fenstr, fen.c_str());
+}
+
--- fen.h
+++ fen.h
@@ -1,3 +1,7 @@
-int is_fen(char *buf);
-int FENtoboard8(Board8x8 board, char *p, int *color, int gametype);
-void board8toFEN(Board8x8 board, char *p, int color, int gametype);
+#pragma once
+#include <string>
+
+int is_fen(const char *buf);
+int FENtoboard8(Board8x8 board, const char *fenstr, int *color, int gametype);
+void board8toFEN(const Board8x8 board, std::string &fenstr, int color, int gametype);
+void board8toFEN(const Board8x8 board, char *fenstr, int color, int gametype);
